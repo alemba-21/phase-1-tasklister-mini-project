94467aa41a64ded461382a66ea6e09b70ff70806
Alemba <alembakiragu@gmail.com>
2022-06-16 17:59:16 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+
+  const taskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("task-description");
+  const newTaskPriority = document.getElementById("new-task-priority");
+
+  let li = document.getElementById("tasks");
+
+  li.addEventListener("click", (e) => {
+    if (e.target.nodeName ==="Button")
+  })
+})
+
+function deleteTask(e){
+  e.target.nodeName.remove()
+}
